5d525560ced202b5b821404ec897a90f115fd722
FoVNull <34230654+FoVNull@users.noreply.github.com>
2020-06-09 00:01:17 +0800

    6.8#2

--- Spider/CtripSpider.py
+++ Spider/CtripSpider.py
@@ -3,10 +3,10 @@ from selenium.common.exceptions import JavascriptException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import pandas as pd
 
 
-def hotelCommentSpider(url: str) -> list:
-    res = []
+def hotelCommentSpider(url: str, id: int):
     driver = webdriver.Ie()
     driver.get(url)
 
@@ -20,7 +20,7 @@ def hotelCommentSpider(url: str) -> list:
     # 规范js代码见 Resources/test.js
 
     sleep(2)
-    arg = "图片"
+    arg = "入住"
     search_js = 'window.document.__webdriver_script_fn = ' \
          '(function () {' \
          'document.getElementById("J_searchInput").value = "' + arg + '";' \
@@ -36,22 +36,42 @@ def hotelCommentSpider(url: str) -> list:
               '})();'
 
     while True:
+        cMap = {"hotel_id": id}
         data = driver.find_element_by_class_name("comment_detail_list")
         strs = str(data.text).split("\n")
         for i in range(len(strs)):
-            if strs[i].__contains__("发表于2"): res.append(strs[i - 1])
+            s = strs[i]
+            if s.__contains__("点评总数"):
+                cMap["comment_num"] = s[4:]
+            if s.__contains__("被点有用"):
+                cMap["useful_num"] = s[4:]
+            if s.__contains__("上传图片"):
+                cMap["pic_num"] = s[4:]
+            if s.__contains__("发表于2"):
+                cMap["date"] = s[3:]
+                cMap["comment"] = strs[i - 1]
+                if strs[i - 2].__contains__("分"): cMap["rating"] = strs[i - 2][0:3]
+                infoList.append(cMap)
+                cMap = {"hotel_id": id}
         try:
             driver.execute_script(page_js)
             sleep(2)
         except JavascriptException as e:
             break
-    # driver.quit()
-    return res
+    driver.quit()
 
 
+def writeCSV():
+    header = ["hotel_id", "comment_num", "useful_num", "pic_num", "date", "rating", "comment"]
+    df = pd.DataFrame(data=infoList, columns=header)
+    print(df)
+    df.to_csv("Resources/comment.csv", encoding='utf8')
+
+
+infoList = []
 if __name__ == '__main__':
-    map = {}
     for i in range(441351, 441352):  # 范围估计在13000~80000，不过这不太重要@_@
         url = "https://hotels.ctrip.com/hotel/"+str(i)+".html"
-        value = hotelCommentSpider(url)
-        if len(value) > 0: map[i] = value
+        hotelCommentSpider(url, i)
+
+    writeCSV()
